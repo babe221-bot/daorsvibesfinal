4471ce6d2b43edde26d06d79149bf51beddc40c6
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-27 04:33:23 +0000

    commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,25 +1,21 @@
-
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/storage';
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxwAQf6RX8UHlbjPpnjMVX-0jS85K3bvw",
-  authDomain: "website-5a18c.firebaseapp.com",
-  projectId: "website-5a18c",
-  storageBucket: "website-5a18c.appspot.com",
-  messagingSenderId: "636702053767",
-  appId: "1:636702053767:web:efb577d6c53d4d06bd6f22",
+    apiKey: "AIzaSyDxwAQf6RX8UHlbjPpnjMVX-0jS85K3bvw",
+    authDomain: "website-5a18c.firebaseapp.com",
+    projectId: "website-5a18c",
+    storageBucket: "website-5a18c.appspot.com",
+    messagingSenderId: "636702053767",
+    appId: "1:636702053767:web:efb577d6c53d4d06bd6f22",
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-const app = firebase.app();
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage();
-
-export { app, auth, db, storage, firebase };
+export { app, auth, db, storage };
