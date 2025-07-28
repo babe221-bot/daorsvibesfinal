4e858786dc60c2211df6c4b134dd5ab5731e5432
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-28 13:05:08 +0000

    c

---
+++ src/app/dashboard/ai-tools/page.tsx
@@ -0,0 +1,8 @@
+
+export default function AiToolsPage() {
+  return (
+    <div>
+      <h1>AI Tools</h1>
+    </div>
+  );
+}
---
+++ src/app/dashboard/playlists/page.tsx
@@ -0,0 +1,8 @@
+
+export default function PlaylistsPage() {
+  return (
+    <div>
+      <h1>Playlists</h1>
+    </div>
+  );
+}
