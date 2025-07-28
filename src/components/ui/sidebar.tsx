9764b0e837c0c9532bc9f0a0e149ada1b09bf8dc
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-28 12:37:13 +0000

    now build me new placeholder for sidebar keed it in proportional and use

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -367,7 +367,7 @@ const SidebarHeader = React.forwardRef<
     <div
       ref={ref}
       data-sidebar="header"
-      className={cn("flex flex-col gap-2", className)}
+      className={cn("flex flex-col gap-2 p-2", className)}
       {...props}
     >
       <div className="flex items-center justify-center">
