7cc3f08001bbd7a429e4f29b66f61d9c71002cc8
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-28 14:02:35 +0000

    commit

---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Logo = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <Image
+        src="/icons/logo-128x128.png"
+        alt="DaorsVibes Logo"
+        width={100}
+        height={100}
+        className="mix-blend-screen"
+      />
+    </div>
+  );
+};
+
+export default Logo;
