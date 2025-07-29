bb5f2820834669c870205c26844692fd761b862b
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-29 01:46:53 +0000

    commit

---
+++ src/app/dashboard/song-library/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react';
+
+const SongLibraryPage = () => {
+  return (
+    <div>
+      <h1>Song Library</h1>
+      {/* Add implementation here */}
+    </div>
+  );
+};
+
+export default SongLibraryPage;
