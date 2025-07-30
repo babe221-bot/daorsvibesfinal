9ae496209df2c28f3e63dadd47deb7a864955f16
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-30 10:36:17 +0000

    CO

--- dataconnect-generated/js/default-connector/index.d.ts
+++ dataconnect-generated/js/default-connector/index.d.ts
@@ -10,3 +10,39 @@ export type DateString = string;
 
 
 
+export interface Album_Key {
+  id: UUIDString;
+  __typename?: 'Album_Key';
+}
+
+export interface ListenEvent_Key {
+  id: UUIDString;
+  __typename?: 'ListenEvent_Key';
+}
+
+export interface PlaylistTrack_Key {
+  playlistId: UUIDString;
+  trackId: UUIDString;
+  __typename?: 'PlaylistTrack_Key';
+}
+
+export interface Playlist_Key {
+  id: UUIDString;
+  __typename?: 'Playlist_Key';
+}
+
+export interface Review_Key {
+  id: UUIDString;
+  __typename?: 'Review_Key';
+}
+
+export interface Track_Key {
+  id: UUIDString;
+  __typename?: 'Track_Key';
+}
+
+export interface User_Key {
+  id: UUIDString;
+  __typename?: 'User_Key';
+}
+
