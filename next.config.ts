14589cc6012f5e115c9a3dd668d58467dd6de725
Bakir Behmen <BakirBehmen@gmail.com>
2025-07-31 16:31:14 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '3000-firebase-studio-1753398910481.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev'],
-=======
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
->>>>>>> 393eea469d735144848945653dca895f2deb8842
   images: {
     remotePatterns: [
       {
