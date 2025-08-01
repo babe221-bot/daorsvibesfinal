389f13722b9110843a58486f1b6957dd41a53acc
Bakir Behmen <BakirBehmen@gmail.com>
2025-08-01 14:04:14 +0000

    committed

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -46,6 +46,9 @@
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = __webpack_module_cache__;
+/******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
@@ -125,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("12884802bee61c29")
+/******/ 		__webpack_require__.h = () => ("5b1975a633406a52")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -215,6 +218,7 @@
 /******/ 	
 /************************************************************************/
 /******/ 	
+/******/ 	// module cache are used so entry inlining is disabled
 /******/ 	
 /******/ })()
 ;
