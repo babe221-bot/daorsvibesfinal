68243b612c95d68f46d68cab7af513e8b3cfebdc
bakirbehmen <bakirbehmen@gmail.com>
2025-08-12 14:44:33 +0000

    commit

--- src/app/dashboard/songs/[id]/page.tsx
+++ src/app/dashboard/songs/[id]/page.tsx
@@ -1,42 +1,43 @@
-    "use client";
-    import React, { useEffect } from 'react';
-    import SongPlayer from '@/components/song-player';
-    import { notFound } from 'next/navigation';
-    import { useAuth } from '@/hooks/use-auth';
-    import { useSong } from '@/hooks/use-song';
-    
-    interface SongPageProps {
-      params: {
-        id: string;
-      };
+"use client";
+import React, { useEffect, use } from 'react';
+import SongPlayer from '@/components/song-player';
+import { notFound } from 'next/navigation';
+import { useAuth } from '@/hooks/use-auth';
+import { useSong } from '@/hooks/use-song';
+
+interface SongPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default function SongPage({ params }: SongPageProps) {
+  const { id } = use(params);
+  const { userId, loading: authLoading, error: authError } = useAuth();
+  const { song, loading: songLoading, error: songError } = useSong(userId, id);
+
+  useEffect(() => {
+    if (!songLoading && !song) {
+      notFound();
     }
-    
-    export default function SongPage({ params }: SongPageProps) {
-      const { userId, loading: authLoading, error: authError } = useAuth();
-      const { song, loading: songLoading, error: songError } = useSong(userId, params.id);
-    
-      useEffect(() => {
-        if (!songLoading && !song) {
-          notFound();
-        }
-      }, [songLoading, song]);
-    
-      if (authLoading || songLoading) {
-        return <div>Loading...</div>;
-      }
-    
-      const error = authError || songError;
-      if (error) {
-        return <div>Error: {error}</div>;
-      }
-    
-      if (!song) {
-        return notFound();
-      }
-    
-      return (
-        <div className="h-screen">
-          <SongPlayer song={song} />
-        </div>
-      );
-    }
+  }, [songLoading, song]);
+
+  if (authLoading || songLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const error = authError || songError;
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!song) {
+    return notFound();
+  }
+
+  return (
+    <div className="h-screen">
+      <SongPlayer song={song} />
+    </div>
+  );
+}
