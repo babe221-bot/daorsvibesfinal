82a9a2473c49fbe111bc9a0187d484a74c1842ed
bakirbehmen <bakirbehmen@gmail.com>
2025-08-12 08:20:25 +0000

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,12 @@
-module.exports = {
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // your existing next config
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '3000-firebase-studio-1753398910481.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev', '9000-firebase-daorsvibesfinalgit-1754936710940.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev', '3001-firebase-daorsvibesfinalgit-1754936710940.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev'],
   images: {
     remotePatterns: [
@@ -9,3 +17,5 @@ module.exports = {
     ],
   },
 }
+
+module.exports = createJestConfig(nextConfig)
