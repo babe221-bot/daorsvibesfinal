fb555d1cc94ae1b0eda229beff4ded052c5c2781
bakirbehmen <bakirbehmen@gmail.com>
2025-08-12 16:39:14 +0000

    commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'daorsvibesfinal',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'daorsvibesfinal',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
