2258f7b9f295f2be0cf36f22744f9098a563dd99
bakirbehmen <bakirbehmen@gmail.com>
2025-08-12 20:50:18 +0000

    commit

--- src/app/dashboard/songs/[id]/page.tsx
+++ src/app/dashboard/songs/[id]/page.tsx
@@ -2,11 +2,7 @@ import SongPlayerPage from './song-player-page';
 import { getSong } from '@/lib/firebase-admin';
 import { notFound } from 'next/navigation';
 
-export default async function SongPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function SongPage({ params }: { params: { id: string } }) {
   const song = await getSong(params.id);
 
   if (!song) {
