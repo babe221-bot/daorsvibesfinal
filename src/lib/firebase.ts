4845bd76b05448a1e763ccf43e6c777b46afd8ef
bakirbehmen <bakirbehmen@gmail.com>
2025-08-14 20:20:41 +0000

    commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, FirebaseApp } from "firebase/app";
+import { getAnalytics, Analytics } from "firebase/analytics";
+import { firebaseConfig } from "./firebase-config";
 
-const firebaseConfig = process.env.NEXT_PUBLIC_FIREBASE_CONFIG 
-    ? JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG) 
-    : {};
 
 // Initialize Firebase
 let app: FirebaseApp;
+let analytics: Analytics;
 if (!getApps().length) {
   app = initializeApp(firebaseConfig);
+  analytics = getAnalytics(app);
 } else {
   app = getApps()[0];
+  analytics = getAnalytics(app);
 }
 
 export default app;
+export { analytics };
