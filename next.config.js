8591df7186df0bc2ec66a4b3bf094106f6859e89
bakirbehmen <bakirbehmen@gmail.com>
2025-08-15 15:59:53 +0000

    commit

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 const nextConfig = {
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '3000-firebase-studio-1753398910481.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev', '9000-firebase-daorsvibesfinalgit-1754936710940.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev', '3001-firebase-daorsvibesfinalgit-1754936710940.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev', '3000-firebase-daorsvibesfinal2-1755200532526.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev', '3001-firebase-daorsvibesfinal2-1755200532526.cluster-fbfjltn3-75c6wqxlhoehbz44sk.cloudworkstations.dev'],
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '3000-firebase-studio-1753398910481.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev', '9000-firebase-daorsvibesfinalgit-1754936710940.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev', '3001-firebase-daorsvibesfinalgit-1754936710940.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev', '3000-firebase-daorsvibesfinal2-1755200532526.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev', '3001-firebase-daorsvibesfinal2-1755200532526.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev'],
   images: {
     remotePatterns: [
       {
