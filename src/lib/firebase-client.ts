09bb39a798fd13ca9b8d7e956d06d7c1806c59fe
bakirbehmen <bakirbehmen@gmail.com>
2025-08-15 17:22:26 +0000

    commit

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -1,28 +1,21 @@
 
-import { initializeApp, getApps, FirebaseApp } from 'firebase/app';
+import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAnalytics, Analytics, isSupported } from 'firebase/analytics';
 import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
 import { firebaseConfig } from './firebase-config';
 
-let app: FirebaseApp;
-let auth: Auth;
-let firestore: Firestore;
+const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth: Auth = getAuth(app);
+const firestore: Firestore = getFirestore(app);
 let analytics: Analytics | undefined;
 
-if (typeof window !== 'undefined' && !getApps().length) {
-  app = initializeApp(firebaseConfig);
-  auth = getAuth(app);
-  firestore = getFirestore(app);
+if (typeof window !== 'undefined') {
   isSupported().then((supported) => {
     if (supported) {
       analytics = getAnalytics(app);
     }
   });
-} else {
-  app = getApps()[0];
-  auth = getAuth(app);
-  firestore = getFirestore(app);
 }
 
 export { app, auth, firestore, analytics };
