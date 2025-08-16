3937a160886ed56c7086808d767102a750a8a549
bakirbehmen <bakirbehmen@gmail.com>
2025-08-16 01:45:44 +0000

    commit

--- functions/lib/genkit-sample.js
+++ functions/lib/genkit-sample.js
@@ -1,12 +1,24 @@
 "use strict";
-var __asyncValues = (this && this.__asyncValues) || function (o) {
-    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
-    var m = o[Symbol.asyncIterator], i;
-    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
-    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
-    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+const __asyncValues = (this && this.__asyncValues) || function(o) {
+  if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+  const m = o[Symbol.asyncIterator]; let i;
+  return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function() {
+    return this;
+  }, i);
+  function verb(n) {
+    i[n] = o[n] && function(v) {
+      return new Promise((resolve, reject) => {
+        v = o[n](v), settle(resolve, reject, v.done, v.value);
+      });
+    };
+  }
+  function settle(resolve, reject, d, v) {
+    Promise.resolve(v).then((v) => {
+      resolve({value: v, done: d});
+    }, reject);
+  }
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {value: true});
 exports.menuSuggestion = void 0;
 // Import the Genkit core libraries and plugins.
 const genkit_1 = require("genkit");
@@ -28,62 +40,63 @@ const apiKey = (0, params_1.defineSecret)("GOOGLE_GENAI_API_KEY");
 const firebase_1 = require("@genkit-ai/firebase");
 (0, firebase_1.enableFirebaseTelemetry)();
 const ai = (0, genkit_1.genkit)({
-    plugins: [
-        // Load the Google AI plugin. You can optionally specify your API key
-        // by passing in a config object; if you don't, the Google AI plugin uses
-        // the value from the GOOGLE_GENAI_API_KEY environment variable, which is
-        // the recommended practice.
-        (0, googleai_1.googleAI)(),
-    ],
+  plugins: [
+    // Load the Google AI plugin. You can optionally specify your API key
+    // by passing in a config object; if you don't, the Google AI plugin uses
+    // the value from the GOOGLE_GENAI_API_KEY environment variable, which is
+    // the recommended practice.
+    (0, googleai_1.googleAI)(),
+  ],
 });
 // Define a simple flow that prompts an LLM to generate menu suggestions.
 const menuSuggestionFlow = ai.defineFlow({
-    name: "menuSuggestionFlow",
-    inputSchema: genkit_1.z.string().describe("A restaurant theme").default("seafood"),
-    outputSchema: genkit_1.z.string(),
-    streamSchema: genkit_1.z.string(),
-}, async (subject, { sendChunk }) => {
-    var _a, e_1, _b, _c;
-    // Construct a request and send it to the model API.
-    const prompt = `Suggest an item for the menu of a ${subject} themed restaurant`;
-    const { response, stream } = ai.generateStream({
-        model: googleai_1.gemini20Flash,
-        prompt: prompt,
-        config: {
-            temperature: 1,
-        },
-    });
+  name: "menuSuggestionFlow",
+  inputSchema: genkit_1.z.string().describe("A restaurant theme").default("seafood"),
+  outputSchema: genkit_1.z.string(),
+  streamSchema: genkit_1.z.string(),
+}, async (subject, {sendChunk}) => {
+  let _a; let e_1; let _b; let _c;
+  // Construct a request and send it to the model API.
+  const prompt = `Suggest an item for the menu of a ${subject} themed restaurant`;
+  const {response, stream} = ai.generateStream({
+    model: googleai_1.gemini20Flash,
+    prompt: prompt,
+    config: {
+      temperature: 1,
+    },
+  });
+  try {
+    for (var _d = true, stream_1 = __asyncValues(stream), stream_1_1; stream_1_1 = await stream_1.next(), _a = stream_1_1.done, !_a; _d = true) {
+      _c = stream_1_1.value;
+      _d = false;
+      const chunk = _c;
+      sendChunk(chunk.text);
+    }
+  } catch (e_1_1) {
+    e_1 = {error: e_1_1};
+  } finally {
     try {
-        for (var _d = true, stream_1 = __asyncValues(stream), stream_1_1; stream_1_1 = await stream_1.next(), _a = stream_1_1.done, !_a; _d = true) {
-            _c = stream_1_1.value;
-            _d = false;
-            const chunk = _c;
-            sendChunk(chunk.text);
-        }
+      if (!_d && !_a && (_b = stream_1.return)) await _b.call(stream_1);
+    } finally {
+      if (e_1) throw e_1.error;
     }
-    catch (e_1_1) { e_1 = { error: e_1_1 }; }
-    finally {
-        try {
-            if (!_d && !_a && (_b = stream_1.return)) await _b.call(stream_1);
-        }
-        finally { if (e_1) throw e_1.error; }
-    }
-    // Handle the response from the model API. In this sample, we just
-    // convert it to a string, but more complicated flows might coerce the
-    // response into structured output or chain the response into another
-    return (await response).text;
+  }
+  // Handle the response from the model API. In this sample, we just
+  // convert it to a string, but more complicated flows might coerce the
+  // response into structured output or chain the response into another
+  return (await response).text;
 });
 exports.menuSuggestion = (0, https_1.onCallGenkit)({
-    // Uncomment to enable AppCheck. This can reduce costs by ensuring only your
-    // Verified app users can use your API. Read more at
-    // https://firebase.google.com/docs/app-check/cloud-functions
-    // enforceAppCheck: true,
-    // authPolicy can be any callback that accepts an AuthData (a uid and tokens
-    // dictionary) and the request data. The isSignedIn() and hasClaim() helpers
-    // can be used to simplify. The following will require the user to have the
-    // email_verified claim, for example.
-    // authPolicy: hasClaim("email_verified"),
-    // Grant access to the API key to this function:
-    secrets: [apiKey],
+  // Uncomment to enable AppCheck. This can reduce costs by ensuring only your
+  // Verified app users can use your API. Read more at
+  // https://firebase.google.com/docs/app-check/cloud-functions
+  // enforceAppCheck: true,
+  // authPolicy can be any callback that accepts an AuthData (a uid and tokens
+  // dictionary) and the request data. The isSignedIn() and hasClaim() helpers
+  // can be used to simplify. The following will require the user to have the
+  // email_verified claim, for example.
+  // authPolicy: hasClaim("email_verified"),
+  // Grant access to the API key to this function:
+  secrets: [apiKey],
 }, menuSuggestionFlow);
-//# sourceMappingURL=genkit-sample.js.map
+// # sourceMappingURL=genkit-sample.js.map
--- functions/lib/index.js
+++ functions/lib/index.js
@@ -7,7 +7,7 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {value: true});
 const firebase_functions_1 = require("firebase-functions");
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -21,9 +21,9 @@ const firebase_functions_1 = require("firebase-functions");
 // functions should each use functions.runWith({maxInstances: 10}) instead.
 // In the v1 API, each function can only serve one request per container, so
 // this will be the maximum concurrent request count.
-(0, firebase_functions_1.setGlobalOptions)({ maxInstances: 10 });
+(0, firebase_functions_1.setGlobalOptions)({maxInstances: 10});
 // export const helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-//# sourceMappingURL=index.js.map
+// # sourceMappingURL=index.js.map
