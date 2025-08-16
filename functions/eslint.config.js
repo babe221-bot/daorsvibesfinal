86eff766fbac04c37ba497a380b1a5ab61bc63c0
bakirbehmen <bakirbehmen@gmail.com>
2025-08-16 04:35:17 +0000

    commit

--- functions/eslint.config.js
+++ functions/eslint.config.js
@@ -2,46 +2,51 @@ const globals = require("globals");
 const pluginJs = require("@eslint/js");
 const tsParser = require("@typescript-eslint/parser");
 const tsPlugin = require("@typescript-eslint/eslint-plugin");
-// It's generally not recommended to require eslint-config packages directly in the flat config format.
-// A common approach is to manually include rules or use community-developed flat configs if available.
-// We will configure rules manually or find a flat-config compatible way to include Google's style guide.
 
 module.exports = [
-  pluginJs.configs.recommended,
-  tsPlugin.configs["eslint-recommended"],
   {
+    files: ["**/*.js"],
     languageOptions: {
-
       globals: {
         ...globals.node,
       },
-      parser: tsParser,
-      parserOptions: {
-        sourceType: "commonjs",
-      },
+      sourceType: "commonjs",
     },
     plugins: {
       js: pluginJs,
-      "@typescript-eslint": tsPlugin,
     },
-    files: ["**/*.js", "**/*.ts"], // Apply this config to both JS and TS files
     rules: {
-      // Recommended JS rules
       ...pluginJs.configs.recommended.rules,
-
-      // Recommended TypeScript rules (overrides or complements JS rules)
-      ...tsPlugin.configs['recommended'].rules,
-
-      // Manually adding rules based on the reported errors and desired style
-      // Rules to address reported errors
       "camelcase": "error",
-      "max-len": ["error", { "code": 120 }], // Increased from 80 to 120
+      "max-len": ["error", { "code": 120 }],
       "no-undef": "error",
       "no-invalid-this": "error",
       "no-var": "error",
       "no-unsafe-finally": "error",
-      "require-jsdoc": "off", // Turning off for now
-
+      "require-jsdoc": "off",
+    },
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parser: tsParser,
+      sourceType: "commonjs",
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs["recommended"].rules,
+      "camelcase": "error",
+      "max-len": ["error", { "code": 120 }],
+      "no-undef": "error",
+      "no-invalid-this": "error",
+      "no-var": "error",
+      "no-unsafe-finally": "error",
+      "require-jsdoc": "off",
     },
   },
 ];
