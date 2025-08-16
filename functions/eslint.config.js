5a6517b48ced1b274e3debd2c25d13a1120cbf83
bakirbehmen <bakirbehmen@gmail.com>
2025-08-16 05:01:50 +0000

    commit

--- functions/eslint.config.js
+++ functions/eslint.config.js
@@ -1,52 +1,16 @@
+
 const globals = require("globals");
 const pluginJs = require("@eslint/js");
-const tsParser = require("@typescript-eslint/parser");
-const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tseslint = require("typescript-eslint");
+
 
 module.exports = [
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    files: ["**/*.js"],
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-      sourceType: "commonjs",
-    },
-    plugins: {
-      js: pluginJs,
-    },
     rules: {
-      ...pluginJs.configs.recommended.rules,
-      "camelcase": "error",
-      "max-len": ["error", { "code": 120 }],
-      "no-undef": "error",
-      "no-invalid-this": "error",
-      "no-var": "error",
-      "no-unsafe-finally": "error",
-      "require-jsdoc": "off",
-    },
-  },
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-      parser: tsParser,
-      sourceType: "commonjs",
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-    },
-    rules: {
-      ...tsPlugin.configs["recommended"].rules,
-      "camelcase": "error",
-      "max-len": ["error", { "code": 120 }],
-      "no-undef": "error",
-      "no-invalid-this": "error",
-      "no-var": "error",
-      "no-unsafe-finally": "error",
-      "require-jsdoc": "off",
+      "@typescript-eslint/no-require-imports": "off",
     },
   },
 ];
