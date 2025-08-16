879f6244d06d6e5e502f351cd57f3b69fbafa9e9
bakirbehmen <bakirbehmen@gmail.com>
2025-08-16 05:56:12 +0000

    commit

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
   // Turbopack configuration for aliasing 'fs' to an empty module
   experimental: {
-    allowedDevOrigins: ['https://3000-firebase-daorsvibesfinal2-1755200532526.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev'],
+    allowedDevOrigins: [
+        'https://3000-firebase-daorsvibesfinal2-1755200532526.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev',
+        'https://3001-firebase-daorsvibesfinal2-1755200532526.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev'
+    ],
     turbo: {
       resolveAlias: {
         'fs': './empty-module.js',
